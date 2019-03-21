877628a5312e415aaf873fa07ad73557486e5d57
Meg Galvez <meg@oakmaine.com>
2019-03-21 17:29:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
- array = string
-  array.each do |word|
-    word.reverse
-  end 
-      
+ string = Array.new 
  
-
-end
+  Array.each do |word|
+    word.reverse
+    
+  end
+  
+end 
